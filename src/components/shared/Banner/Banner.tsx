89d51ad7e0bf744853506f9977011344bfb888e9
Jamil-Aktar-Ifat <jamilaktarifat@hotmail.com>
2025-08-17 13:19:30 +0100

    blogs api fetched and revalidating implemented

--- src/components/shared/Banner/Banner.tsx
+++ src/components/shared/Banner/Banner.tsx
@@ -2,8 +2,8 @@ import styles from "./Banner.module.css";
 const Banner = () => {
   return (
     <div className={styles.banner_container}>
-      <div className="container h-full flex justify-center items-center">
-        <div className="w-[60%] h-[60%] rounded-xl text-center bg-white px-16 py-20">
+      <div className="container h-full flex justify-center items-center mx-auto">
+        <div className="w-[60%] h-[60%] rounded-md  text-center bg-white px-16 py-20">
           <p className="w-40 mx-auto rounded-full p-2 my-5 bg-teal-100 text-teal-600">
             1st January 2025
           </p>
