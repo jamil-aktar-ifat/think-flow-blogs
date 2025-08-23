85174f0df507440214353ae417e3cb162fc423bb
Jamil-Aktar-Ifat <jamilaktarifat@hotmail.com>
2025-08-23 19:27:12 +0100

    implemented form using Next.Js Form component

---
+++ src/app/blogs/create/page.tsx
@@ -0,0 +1,11 @@
+import BlogForm from "@/components/ui/BlogForm";
+
+const CreateBlogPage = () => {
+  return (
+    <div>
+      <BlogForm></BlogForm>
+    </div>
+  );
+};
+
+export default CreateBlogPage;
